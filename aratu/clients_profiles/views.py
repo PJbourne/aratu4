25b1c9b96f265d084916899fd5fbf4780049fa3c
Pablo Monteiro <musicbas.3107@gmail.com>
2025-01-16 11:16:42 -0300

    primeiras alteracoes

--- aratu/clients_profiles/views.py
+++ aratu/clients_profiles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+#from django.shortcuts import render
+
+def home(request):
+    return HttpResponse("<h1>Bem-vindo à Home!</h1>")
