ba63b2e23d28a46fd82d1143b04c0037a1d9473c
Pablo Monteiro <musicbas.3107@gmail.com>
2025-01-16 23:08:01 -0300

    n sei. se continuar dando ruim, retorno p --v limpa

--- aratu/clients_profiles/apps.py
+++ aratu/clients_profiles/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class ClientsProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'clients_profiles'
+    def ready(self):
+        import clients_profiles.signals
--- aratu/clients_profiles/models.py
+++ aratu/clients_profiles/models.py
@@ -4,4 +4,14 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
-    
+#daqui pra baixo foi add por pablo depois
+"""
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    birthdate = models.DateField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return f"Profile of {self.user.username}"
+
+"""
